b1aa3711ed6867618bae498d392fd8f81cee99fc
lyj00912 <3122742760@qq.com>
2024-05-22 09:29:03 +0800

    used custom font ttf.

--- SubUIProcess.py
+++ SubUIProcess.py
@@ -1,8 +1,10 @@
 import multiprocessing
 import tkinter as tk
 from tkinter import scrolledtext
+import tkinter.font as tk_font
 import time
 import ctypes
+import pyglet
 
 
 def GUIProcess(pipe_conn):
@@ -16,8 +18,16 @@ def GUIProcess(pipe_conn):
     root = tk.Tk()
     root.title("Text Display")
 
+    # 使用pyglet加载自定义字体
+    pyglet.options['win32_gdi_font'] = True
+    font_path = "./ChineseSupport/SourceHanSans_Normal.ttf"
+    pyglet.font.add_file(str(font_path))
+
+    # 创建字体对象
+    font = tk_font.Font(family="Source Han Sans Normal", size=24)
+
     # 创建一个ScrolledText小部件来显示文本
-    text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=20)
+    text_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=15, font=font)
     text_area.pack(padx=10, pady=10, expand=True, fill=tk.BOTH)
 
     def check_for_messages():
