e1813ceb0977ee19185f7ff1982b79d94d88fb2c
lyj00912 <3122742760@qq.com>
2024-06-20 14:21:33 +0800

    updated VENV tools.

--- VENV_EXTRACT.bat
+++ VENV_EXTRACT.bat
@@ -3,6 +3,14 @@
 chcp 65001
 cls
 
+echo "---------- EXTRACT ----------"
+echo "确保你把本VENV_EXTRACT.bat程序，放在和虚拟环境的Scripts文件夹同一个目录下"
+echo "准备好了，按任意键开始提取..."
+pause > nul
+
+echo "---------- EXTRACT ----------"
+echo "正在提取中..."
+
 call .\Scripts\activate.bat
 
 pip freeze > requirements.txt
@@ -11,5 +19,6 @@ pip download -r requirements.txt -d ./wheels
 echo "---------- FINISHED ----------"
 echo "提取产物：requirements.txt清单 + wheels文件夹"
 echo "请把这两个东西保存好，可以用来重建venv"
-echo "按任意键退出..."
+echo "按任意键退出程序..."
 pause > nul
+exit
